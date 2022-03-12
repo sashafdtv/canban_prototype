4188e9a2b11a01dbfd335e025332891ae79e5916
afdtv <fedotov.a@roistat.com>
2022-03-12 23:17:56 +0300

    remove bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <div class="controls">
+      <input v-model="newTask" placeholder="Enter Task" @keyup.enter="add" />
+    </div>
+
+    <div class="boards">
+      <div class="task">
+        <h3>Backlog</h3>
+        <draggable :list="arrBacklog">
+          <div v-for="element in arrBacklog" :key="element.name">
+            {{ element.name }}
+          </div>
+        </draggable>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import draggable from "vuedraggable";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  components: { draggable },
+  data() {
+    return {
+      newTask: "",
+      arrBacklog: [
+        { name: "Code SignPage" },
+        { name: "Test Dashboard" },
+        { name: "Style Registration" },
+        { name: "Help with Designs" },
+      ],
+      arrInProgress: [],
+      arrTested: [],
+      arrDone: [],
+    };
+  },
+  methods: {
+    add() {
+      if (this.newTask) {
+        this.arrBacklog.push({ name: this.newTask });
+        this.newTask;
+      }
+    },
   },
 };
 </script>
